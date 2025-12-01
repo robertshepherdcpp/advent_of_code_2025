6401e1d7df4b64a9e3355daa3bef208ca2834e09
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2025-12-01 21:27:46 +0000

    add some stuff - didnt get to second part but know how to do it.

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,1 +1,54 @@
+#include<iostream> // std::cout
+#include<fstream> // std::fstream
+#include<string> // std::string
 
+auto main() -> int {
+    std::ifstream file("file.txt");
+    std::string s{};
+    int zero_count = 0;
+    int index = 50;
+    while(file >> s) {
+        std::cout << "Currently at index: " << index << "\n";
+        int integer = std::stoi(s.substr(1));
+        // std::cout << integer << "\n";
+        // if(s[0] == 'L') {
+        //     if(index-integer < 0) {
+        //         index = 99 - (((index-integer) * -1) % 100) + 1;
+        //     }
+        //     else {
+        //         index -= integer;
+        //     }
+        // } else { // s[0] == 'R'
+        //     if(integer+index > 99) {
+        //         index = (integer+index) % 100 - 1;
+        //     }
+        //     else {
+        //         index += integer;
+        //     }
+        // }
+        if (s[0] == 'L') {
+            int count = ((index-integer)*-1 - (index+integer)%100) / 100;
+            zero_count += count;
+            index = (index - integer) % 100;
+            if (index < 0) index += 100;
+        } else { // R
+            if(index + integer < 99)
+            {
+                index = (index + integer);
+            }
+            else {
+                int count = ((index+integer) - (index+integer)%100) / 100;
+                zero_count += count;
+
+                index = (index + integer) % 100;
+            }
+        }
+        if(index == 0) {
+            zero_count += 1;
+        }
+    }
+
+    std::cout << zero_count << "\n";
+
+    return {};
+}
