69b20241a8ace2edbd6b0bfc04469328667d8c28
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2025-12-03 13:39:17 +0000

    add some more

--- day3/main.cpp
+++ day3/main.cpp
@@ -2,12 +2,25 @@
 #include<fstream> // std::fstream
 #include<string> // std::string
 
+auto find_largest(std::string s) -> std::pair<char, int> {
+    std::pair<char, int> smallest{};
+    for(int i = 0; i < s.size()-1; i++) {
+        if(s[i] > smallest.first) {
+            smallest = {s[i], i};
+        }
+    }
+    return smallest;
+}
+
 auto main() -> int {
     std::ifstream file("file.txt");
-    std::string s{};
+    std::string line{};
     int total_count = 0;
-    while (file >> s) {
+    while (file >> line) {
         // line by line
+        auto largest = find_largest(line);
+        auto next_largest = find_largest(line.substr(largest.second+1));
+        total_count += (largest.first - '0') * 10 + (next_largest.first - '0');
     }
 
     std::cout << total_count << "\n";
