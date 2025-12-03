ae15b9869145ad6e674055afd4d9c4a5e58bd03d
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2025-12-03 10:36:19 +0000

    add start - still needs finishing

--- day3/main.cpp
+++ day3/main.cpp
@@ -1,1 +1,16 @@
+#include<iostream> // std::cout
+#include<fstream> // std::fstream
+#include<string> // std::string
 
+auto main() -> int {
+    std::ifstream file("file.txt");
+    std::string s{};
+    int total_count = 0;
+    while (file >> s) {
+        // line by line
+    }
+
+    std::cout << total_count << "\n";
+
+    return {};
+}
