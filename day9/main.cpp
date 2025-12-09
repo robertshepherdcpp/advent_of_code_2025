a2ef32840daed81d4d177a7bacbd74a1c78b47d5
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2025-12-09 10:36:27 +0000

    add some intuition

---
+++ day9/main.cpp
@@ -0,0 +1,7 @@
+/*
+First of all, what i need to do:
+- for each 2 points in the input we need to find the ones with the largest area between them
+- there is obviously the brute force way of doing things
+- this would be o(^2) time
+
+*/
